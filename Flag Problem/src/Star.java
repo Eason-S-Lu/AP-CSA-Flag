7e6345c160ccf83a700e53e75c5aa99b6f3f4d6f
Eason Lu <git@eason.email>
2023-08-21 21:30:24 -0700

    change star class so the input is deg

--- Flag Problem/src/Star.java
+++ Flag Problem/src/Star.java
@@ -3,19 +3,18 @@ public class Star{
 	int xcord[] = new int[10];
 	int ycord[] = new int[10];
 	public void draw(double xoffset, double yoffset, double outerRadius, double innerRadius, Graphics g){
-		Graphics2D g2d = (Graphics2D) g;
 		int i = 0;
 		for (int deg = 18 ; deg <=360 ; deg += 36){
 			if (i%2 == 0){
-				xcord[i] = (int) Math.round(xoffset + (outerRadius * Math.cos(Math.toRadians(18))));
-				ycord[i] = (int) Math.round(yoffset - (outerRadius * Math.sin(Math.toRadians(18))));
+				xcord[i] = (int) Math.round(xoffset + (outerRadius * Math.cos(Math.toRadians(deg))));
+				ycord[i] = (int) Math.round(yoffset - (outerRadius * Math.sin(Math.toRadians(deg))));
 			}
 			else{
-				xcord[i] = (int) Math.round(xoffset + (innerRadius * Math.cos(Math.toRadians(54))));
-				ycord[i] = (int) Math.round(yoffset - (innerRadius * Math.sin(Math.toRadians(54))));
+				xcord[i] = (int) Math.round(xoffset + (innerRadius * Math.cos(Math.toRadians(deg))));
+				ycord[i] = (int) Math.round(yoffset - (innerRadius * Math.sin(Math.toRadians(deg))));
 			}
 			i++;
 		}
-		g2d.fillPolygon(xcord, ycord, 10);
+		g.fillPolygon(xcord, ycord, 10);
 	}
 }
